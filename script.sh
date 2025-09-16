a5eaf5f7dfa5f9ac2407645ac5b66ea020a4f9d8
FARHAN MUH TASIM <frnbuid2005@gmail.com>
2025-09-16 17:29:20 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,7 +5,7 @@ rm -rf android_device_xiaomi_daisy
 
 
 # Initialize local repository
-repo init -u https://github.com/crdroidandroid/android.git -b 15.0 --git-lfs
+repo init -u https://github.com/Project-Mist-OS/manifest -b vic --git-lfs
 echo "=================="
 echo "Repo init success"
 echo "=================="
@@ -36,7 +36,7 @@ echo "======= Export Done ======"
 #rm -rf packages/apps/Settings
 #git clone https://github.com/frnwot/android_packages_apps_Settings_crd.git -b 15.0 packages/apps/Settings
 rm -rf device/xiaomi/daisy
-git clone https://github.com/msm8953F/android_device_xiaomi_daisy -b Axion-qpr2 device/xiaomi/daisy
+git clone https://github.com/msm8953F/android_device_xiaomi_daisy -b mist-15 device/xiaomi/daisy
 
 # Set up build environment
 source build/envsetup.sh
@@ -44,6 +44,7 @@ echo "====== Envsetup Done ======="
 
 # Lunch
 . build/envsetup.sh
-brunch daisy
+mistify daisy user 
+mist sb
 
 
